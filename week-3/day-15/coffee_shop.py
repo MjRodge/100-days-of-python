22296227e75ad7b3348f29035bedaca5ade3240d
mjrod <michael@1mrkt.to>
2021-11-30 19:46:15 -0500

    coffee_shop print resources

--- week-3/day-15/coffee_shop.py
+++ week-3/day-15/coffee_shop.py
@@ -1,13 +1,17 @@
-from coffee_recipes import MENU
+from coffee_recipes import MENU, resources
 
 
 def take_customer_order():
     customer_selection = input("what drink? espresso/latte/cappuccino: ").lower()
     print(customer_selection)
 
+def print_resources(current_resources):
+    print(current_resources)
+
 def make_a_coffee():
     print("coffee, yum")
     take_customer_order()
+    print_resources(resources)
 
 make_a_coffee()
 
