30aa5f93fe9f6f6bc3abaf069657f8b4b4964c11
mjrod <michael@1mrkt.to>
2021-11-30 19:36:12 -0500

    coffee_shop got user input

--- week-3/day-15/coffee_shop.py
+++ week-3/day-15/coffee_shop.py
@@ -1,3 +1,13 @@
 from coffee_recipes import MENU
 
-print(MENU)
+
+def take_customer_order():
+    customer_selection = input("what drink? espresso/latte/cappuccino: ").lower()
+    print(customer_selection)
+
+def make_a_coffee():
+    print("coffee, yum")
+    take_customer_order()
+
+make_a_coffee()
+
