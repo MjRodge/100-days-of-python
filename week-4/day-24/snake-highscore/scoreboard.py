f9dab1748833065a25531da9e10f6bc70a48a644
mjrod <michael@1mrkt.to>
2022-01-19 12:40:08 -0500

    day twenty four - mail merge

--- week-4/day-24/snake-highscore/scoreboard.py
+++ week-4/day-24/snake-highscore/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as high_score_file:
+            self.high_score = int(high_score_file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
