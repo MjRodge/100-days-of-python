a0db1cf48ed4c9a0eeb7cfd3c571b9091dc33ea9
mjrod <michael@1mrkt.to>
2022-01-31 11:41:02 -0500

    day-26 - filtering even numbers with list comprehension

--- week-4/day-26/even_numbers.py
+++ week-4/day-26/even_numbers.py
@@ -3,8 +3,8 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 #Write your 1 line code 👇 below:
 
-
+even_numbers = [number for number in numbers if number % 2 == 0]
 
 #Write your code 👆 above:
 
-print(result)
+print(even_numbers)
