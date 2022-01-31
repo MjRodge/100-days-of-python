66096168d981876fa99b347b401b0cc8d9bf89c0
mjrod <michael@1mrkt.to>
2022-01-31 13:11:02 -0500

    day-26 - filtering duplicates with list comprehension

--- week-4/day-26/data-overlap/main.py
+++ week-4/day-26/data-overlap/main.py
@@ -1,4 +1,20 @@
 
+# create function to prepare file contents of each file into array
+def file_handler(file):
+    with open(file) as file_contents:
+        file = file_contents.readlines()
+    file_array = []
+    for item in file:
+        trimmed_item = item.strip("\n")
+        file_array.append(int(trimmed_item))
+    return file_array
+
+
+# open .txt files and read contents into a list
+file1 = file_handler("file1.txt")
+file2 = file_handler("file2.txt")
+
+result = [item for item in file1 if item in file2]
 
 # Write your code above 👆
 
