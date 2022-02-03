cc5fe571fe1dca7b3e0fbf5c6f818636a86b3b1b
mjrod <michael@1mrkt.to>
2022-02-03 13:07:43 -0500

    day-27 - tkinter and *args

--- week-4/day-27/tkinter-first.py
+++ week-4/day-27/tkinter-first.py
@@ -1,10 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+# windows
+window = Tk()
 window.title("this is my first gui")
 window.minsize(width=800, height=500)
+window.config(padx=50, pady=50)
 
-my_label = tkinter.Label(text="this is a label", font=("Arial", 24, "bold"))
-my_label.pack()
+# labels
+my_label = Label(text="this is a label", font=("Arial", 24, "bold"))
+my_label.grid(row=0, column=0)
+
+
+# buttons
+def button_click():
+    my_label.config(text=my_input.get())
+
+
+my_button = Button(text="click me", command=button_click)
+my_button.grid(row=1, column=1)
+
+new_button = Button(text="new button", command=button_click)
+new_button.grid(row=0, column=2)
+# entry
+my_input = Entry()
+my_input.grid(row=2, column=3)
+
+
 
 window.mainloop()
