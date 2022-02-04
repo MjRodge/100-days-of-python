b066005efd7953972461e88a8c4f71a030a12be8
mjrod <michael@1mrkt.to>
2022-02-04 11:48:42 -0500

    day-28 - pomodoro clock countdown mechanism

--- week-4/day-28/main.py
+++ week-4/day-28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,8 +14,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    countdown(5*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+
+    canvas.itemconfig(canvas_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count-1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,18 +34,13 @@ window.config(pady=100, padx=100, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 18, "bold"))
+canvas_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 18, "bold"))
 canvas.grid(row=1, column=1)
 
 timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 20, "bold"))
 timer_label.grid(row=0, column=1)
 
-
-def clicked_start():
-    pass
-
-
-start_button = Button(text="start", command=clicked_start)
+start_button = Button(text="start", command=start_timer)
 start_button.grid(row=2, column=0)
 
 
