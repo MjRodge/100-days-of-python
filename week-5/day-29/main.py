e6c35ff249624e90fd8934f80f66c97cb2387530
mjrod <michael@1mrkt.to>
2022-02-07 15:49:26 -0500

    day-29 - password manager, configure UI elements

--- week-5/day-29/main.py
+++ week-5/day-29/main.py
@@ -20,24 +20,26 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="website: ")
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=35)
+website_entry.focus()
 website_entry.grid(row=1, column=1, columnspan=2)
 
 # configure email/username UI elements
 username_label = Label(text="email/username: ")
 username_label.grid(row=2, column=0)
 username_entry = Entry(width=35)
+username_entry.insert(0, "test@email.com")
 username_entry.grid(row=2, column=1, columnspan=2)
 
 # configure password UI elements
-password_label = Label(text="website: ")
+password_label = Label(text="password: ")
 password_label.grid(row=3, column=0)
-password_entry = Entry(width=21)
+password_entry = Entry(width=18)
 password_entry.grid(row=3, column=1)
 password_button = Button(text="generate password")
 password_button.grid(row=3, column=2)
 
 # configure submit button UI element
-submit_button = Button(text="add")
+submit_button = Button(text="add", width=33)
 submit_button.grid(row=4, column=1, columnspan=2)
 
 
