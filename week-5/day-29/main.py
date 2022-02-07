4633d86bea78c4a356d051ede40cebfc78c9b858
mjrod <michael@1mrkt.to>
2022-02-07 12:26:52 -0500

    day-29 - password manager, configure canvas/img

--- week-5/day-29/main.py
+++ week-5/day-29/main.py
@@ -1,5 +1,22 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# configure window
+window = Tk()
+window.title("password-manager")
+window.config(pady=100, padx=100)
+
+
+# configure img canvas
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=1, column=1)
+
+
+
+window.mainloop()
