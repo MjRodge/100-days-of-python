77b79c58627a6f5983b1cdc8b0fbb6ca3b5f62e0
mjrod <michael@1mrkt.to>
2022-02-08 10:19:09 -0500

    day-29 - password manager, refactored password-gen to use list comprehension

--- week-5/day-29/main.py
+++ week-5/day-29/main.py
@@ -1,26 +1,20 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-# Password Generator Project
-import random
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
-
 password_list = []
-selected_letters = [random.choice(letters) for n in range(0, nr_letters)]
-selected_symbols = [random.choice(symbols) for n in range(0, nr_symbols)]
-selected_numbers = [random.choice(numbers) for n in range(0, nr_numbers)]
+selected_letters = [choice(letters) for n in range(0, randint(8, 10))]
+selected_symbols = [choice(symbols) for n in range(0, randint(2, 4))]
+selected_numbers = [choice(numbers) for n in range(0, randint(2, 4))]
 password_list = selected_letters + selected_symbols + selected_numbers
-random.shuffle(password_list)
+shuffle(password_list)
 generated_password = ""
 for char in password_list:
     generated_password += char
