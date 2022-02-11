3569b3a5c322a724c0b7aef89b195a2bc638baf4
mjrod <michael@1mrkt.to>
2022-02-11 14:01:35 -0500

    day-32 - email quotes, refactored smtp connection

--- week-5/day-32/quote-delivery/quote_delivery.py
+++ week-5/day-32/quote-delivery/quote_delivery.py
@@ -1,9 +1,8 @@
 import smtplib
 from secrets import my_email, my_password
 
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg="Subject:second testing from python\n\nThis is the body of the email, i hope it works!")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg="Subject:third testing from python\n\nThis is the body of the email, i hope it works!")
 
