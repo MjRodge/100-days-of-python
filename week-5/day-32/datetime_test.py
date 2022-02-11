ae6a241b765b6b8e2f5286b83887d01ee413cdde
mjrod <michael@1mrkt.to>
2022-02-11 12:33:53 -0500

    day-32 - using the datetime module

--- week-5/day-32/datetime_test.py
+++ week-5/day-32/datetime_test.py
@@ -5,4 +5,7 @@ year = now.year
 month = now.month
 day = now.day
 
+date_of_birth = dt.datetime(year=1991, month=10, day=31)
+
+print(date_of_birth)
 print(f"year: {year} month: {month} day: {day}")
