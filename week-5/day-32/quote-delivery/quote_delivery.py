6244470112752aedbac4d2997e096d9369faf126
mjrod <michael@1mrkt.to>
2022-02-13 11:35:27 -0500

    day-32 - email quotes, changed file read to pandas

--- week-5/day-32/quote-delivery/quote_delivery.py
+++ week-5/day-32/quote-delivery/quote_delivery.py
@@ -1,4 +1,5 @@
 import smtplib
+import pandas
 from secrets import my_email, my_password
 
 # configure smtp connection with gmail account
@@ -6,9 +7,8 @@ with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.starttls()
     # my_email my_password configured in secrets.py
     connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg="Subject:third testing from python\n\nThis is the body of the email, i hope it works!")
+    # connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg="Subject:third testing from python\n\nThis is the body of the email, i hope it works!")
 
-with open("quotes.csv", "r") as quote_contents:
-    quotes = quote_contents.readlines()
+quotes = pandas.read_csv("quotes.csv")
 
 print(quotes)
