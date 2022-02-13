64c4d77e7d4a10b23c8419f8976ba00a1e3fba5a
mjrod <michael@1mrkt.to>
2022-02-13 12:06:35 -0500

    day-32 - email quotes, use pandas to generate single, random quote

--- week-5/day-32/quote-delivery/quote_delivery.py
+++ week-5/day-32/quote-delivery/quote_delivery.py
@@ -9,6 +9,11 @@ with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.login(user=my_email, password=my_password)
     # connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg="Subject:third testing from python\n\nThis is the body of the email, i hope it works!")
 
+# load quotes from csv
 quotes = pandas.read_csv("quotes.csv")
-
-print(quotes)
+# find a single random quote using pandas sample() function
+random_quote = quotes.sample()
+# assign the author and quote text to variables
+author = random_quote["Author"].loc[random_quote.index[0]]
+quote_text = random_quote["Quote"].loc[random_quote.index[0]]
+print(f"author: {author}, message: {quote_text}")
