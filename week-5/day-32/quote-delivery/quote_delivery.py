b912d7505f323f1d61121181d1ad788cbe351ab9
mjrod <michael@1mrkt.to>
2022-02-14 08:27:57 -0500

    day-32 - email quotes, added conditional to send only on monday

--- week-5/day-32/quote-delivery/quote_delivery.py
+++ week-5/day-32/quote-delivery/quote_delivery.py
@@ -3,12 +3,15 @@ import pandas
 import datetime as dt
 from secrets import my_email, my_password
 
+
 # configure smtp connection with gmail account
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    # my_email my_password configured in secrets.py
-    connection.login(user=my_email, password=my_password)
-    # connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg="Subject:third testing from python\n\nThis is the body of the email, i hope it works!")
+def send_mail():
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        # my_email my_password configured in secrets.py
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg=f"Subject:python quote\n\nauthor: {author}, message: {quote_text}")
+
 
 # load quotes from csv
 quotes = pandas.read_csv("quotes.csv")
@@ -17,7 +20,10 @@ random_quote = quotes.sample()
 # assign the author and quote text to variables
 author = random_quote["Author"].loc[random_quote.index[0]]
 quote_text = random_quote["Quote"].loc[random_quote.index[0]]
-print(f"author: {author}, message: {quote_text}")
 
 now = dt.datetime.now()
-print(now)
+weekday = now.weekday()
+
+# send message on Monday (weekday == 0)
+if weekday == 0:
+    send_mail()
