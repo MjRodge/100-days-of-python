a526af0fdbd475a7400557aa648995c4550ef0e0
mjrod <michael@1mrkt.to>
2022-02-14 12:43:53 -0500

    day-35 - weather alert, used weather id to print when rain is expected

--- week-5/day-35/openweathermap_api_request.py
+++ week-5/day-35/openweathermap_api_request.py
@@ -10,5 +10,9 @@ params = {
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=params)
 response.raise_for_status()
 data = response.json()
-print(response.status_code)
-print(data)
+# print(data)
+
+hourly_forecast = data["hourly"]
+for x in range(0, 12):
+    if hourly_forecast[x]["weather"][0]["id"] < 700:
+        print("rain, bring an umbrella")
