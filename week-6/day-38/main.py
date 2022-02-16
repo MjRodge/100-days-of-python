0073e12c6b927322479edfdb126272e484eaabb8
mjrod <michael@1mrkt.to>
2022-02-16 14:21:46 -0500

    day-38 - workout-tracking formatted date/time for sheety api submission

--- week-6/day-38/main.py
+++ week-6/day-38/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, time
 from keys import NUTRITIONIX_API_KEY, NUTRITIONIX_APP_ID
 
 # take input from user to pass to nutritionix api
@@ -26,3 +27,12 @@ response.raise_for_status()
 # returned data from nutritionix api
 data = response.json()
 print(data)
+
+# using python datetime to build date/time variables in correct format for submission to sheety api
+date = datetime.now()
+year = date.strftime("%Y")
+month = date.strftime("%m")
+day = date.strftime("%d")
+formatted_date = f"{day}/{month}/{year}"
+time = date.strftime("%H:%M:%S")
+
