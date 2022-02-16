319d7eb6fea02df496e4ccb01ec6d74a146658ac
mjrod <michael@1mrkt.to>
2022-02-16 14:07:33 -0500

    day-38 - workout-tracking added comments

--- week-6/day-38/main.py
+++ week-6/day-38/main.py
@@ -1,14 +1,17 @@
 import requests
 from keys import NUTRITIONIX_API_KEY, NUTRITIONIX_APP_ID
 
+# take input from user to pass to nutritionix api
+# eg: "i walked 7km and ran 3km"
 exercise = input("enter the activities that you did: ")
 
+# authorisation headers for nutritionix api
 headers = {
     "x-app-id": NUTRITIONIX_APP_ID,
     "x-app-key": NUTRITIONIX_API_KEY,
     "x-remote-user-id": "0"
 }
-
+# parameters to pass to nutritionix api
 params = {
     "query": exercise,
     "gender": "male",
@@ -17,8 +20,9 @@ params = {
     "age": 30
 }
 
+# send request/auth/params to nutritionix api
 response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", headers=headers, json=params)
 response.raise_for_status()
-
+# returned data from nutritionix api
 data = response.json()
 print(data)
