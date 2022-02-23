c6f29b31d1d445b796fa2e98d37eeb6ca853c21c
mjrod <michael@1mrkt.to>
2022-02-23 09:57:50 -0500

    day 39+40 - flight-club gave headers for tequila api global scope to reduce duplication

--- week-6/day-39+40/flight_search.py
+++ week-6/day-39+40/flight_search.py
@@ -4,6 +4,10 @@ TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_AITA_QUERY = "/locations/query"
 TEQUILA_LOCATION_QUERY = "/search"
 
+headers = {
+            "apikey": TEQUILA_API
+        }
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def __init__(self, city_name):
@@ -14,13 +18,12 @@ class FlightSearch:
             "term": self.city_name,
             "location_types": "city"
         }
-        headers = {
-            "apikey": TEQUILA_API
-        }
         response = requests.get(url=f"{TEQUILA_ENDPOINT}{TEQUILA_AITA_QUERY}", params=payload, headers=headers)
         response.raise_for_status()
         returned_data = response.json()["locations"]
         return returned_data[0]["code"]
+
+
 
 
 
