657fe428fc9724171fe88c79fd1497b34e88fd74
mjrod <michael@1mrkt.to>
2022-02-25 12:41:07 -0500

    day-45 - web scraping beautifulsoup selectors

--- week-7/day-45/main.py
+++ week-7/day-45/main.py
@@ -6,3 +6,11 @@ with open("website.html") as website:
 
 soup = BeautifulSoup(website_content, "html.parser")
 print(soup.prettify())
+
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+for tag in all_anchor_tags:
+    print(tag.get("href"))
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
