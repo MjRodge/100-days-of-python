d8fc9f8886d5a65b40abd4d263f455460a733c2a
mjrod <michael@1mrkt.to>
2022-02-25 12:33:39 -0500

    day-45 - web scraping used beautifulsoup to print html file

--- week-7/day-45/main.py
+++ week-7/day-45/main.py
@@ -1,3 +1,8 @@
+from bs4 import BeautifulSoup
+
 with open("website.html") as website:
     website_content = website.read()
-    print(website_content)
+    # print(website_content)
+
+soup = BeautifulSoup(website_content, "html.parser")
+print(soup.prettify())
