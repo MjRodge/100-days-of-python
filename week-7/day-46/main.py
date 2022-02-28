7623ea9b9b868a9696ec8bfa629d849a0d3c31f4
mjrod <michael@1mrkt.to>
2022-02-28 09:53:35 -0500

    day-46 - web-scraping billboard-100 scrape 99 top song titles to variable

--- week-7/day-46/main.py
+++ week-7/day-46/main.py
@@ -9,4 +9,5 @@ response = requests.get(f"{BILLBOARD_URL}/{target_date}")
 chart_html = response.text
 
 chart_soup = BeautifulSoup(chart_html, "html.parser")
-print(chart_soup)
+song_titles = chart_soup.find_all(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
+print(song_titles)
