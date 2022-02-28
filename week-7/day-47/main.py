8c92c2d49881aafd17b162fd3ab045496a4a3659
mjrod <michael@1mrkt.to>
2022-02-28 15:36:31 -0500

    day-47 - web-scraping amazon-price-tracker prepared smtplib module for sending email

--- week-7/day-47/main.py
+++ week-7/day-47/main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+from keys import FROM_ADDR, TO_ADDR, EMAIL_PASSWORD
 
 AMAZON_URL = "https://www.amazon.ca/Apple-MacBook-14-inch-8‑core-14‑core/dp/B09JQSLL92/ref=sr_1_1"
 
@@ -7,12 +9,24 @@ HEADERS = {
     "Accept-Language": "en-GB,en-US;q=0.7,en;q=0.3",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98."
 }
+# set the lowest price for the user to be emailed a notification
+PRICE_THRESHOLD = 2600
 
+# get the data from amazon url
 response = requests.get(url=AMAZON_URL, headers=HEADERS)
 amazon_html = response.text
 amazon_soup = BeautifulSoup(amazon_html, "html.parser")
+# pull out dollar/cent value of item and combine into a variable, stripping unnecessary characters
 dollars = amazon_soup.find(name="span", class_="a-price-whole").get_text().replace(",", "")
 cents = amazon_soup.find(name="span", class_="a-price-fraction").get_text()
 price = float(f"{dollars}{cents}")
+
+message = "Subject: Amazon\n\nmessage body"
+
+
+email_connection = smtplib.SMTP("smtp.gmail.com", 587)
+email_connection.starttls()
+email_connection.login(user=FROM_ADDR, password=EMAIL_PASSWORD)
+email_connection.sendmail(from_addr=FROM_ADDR, to_addrs=TO_ADDR, msg=message)
 
 print(type(price))
