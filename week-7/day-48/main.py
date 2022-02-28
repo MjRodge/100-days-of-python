d519def44ef8e6da9aea4b31b3ec760611c89d0c
mjrod <michael@1mrkt.to>
2022-02-28 17:07:09 -0500

    day-48 - web-scraping use selenium to scrape all events from python.org

--- week-7/day-48/main.py
+++ week-7/day-48/main.py
@@ -6,8 +6,13 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://python.org")
 search = driver.find_element_by_name("q")
-print(search.get_attribute("placeholder"))
+# print(search.get_attribute("placeholder"))
 
 documentation_link = driver.find_element_by_css_selector(".documentation-widget a")
-print(documentation_link.text)
+# print(documentation_link.text)
+
+python_events = driver.find_elements_by_xpath("/html/body/div/div[3]/div/section/div[2]/div[2]/div/ul/li")
+for x in python_events:
+    print(x.text)
+
 driver.quit()
