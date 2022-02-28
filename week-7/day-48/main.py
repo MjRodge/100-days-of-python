e8c8eedae1cb10839e930f5d7801ca35e975d167
mjrod <michael@1mrkt.to>
2022-02-28 16:51:35 -0500

    day-48 - web-scraping use selenium to scrape by css selector

--- week-7/day-48/main.py
+++ week-7/day-48/main.py
@@ -7,4 +7,7 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://python.org")
 search = driver.find_element_by_name("q")
 print(search.get_attribute("placeholder"))
+
+documentation_link = driver.find_element_by_css_selector(".documentation-widget a")
+print(documentation_link.text)
 driver.quit()
