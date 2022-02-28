e608f887482127153460daa142b3e92a5d25f9d1
mjrod <michael@1mrkt.to>
2022-02-28 16:48:49 -0500

    day-48 - web-scraping use selenium to scrape attribute

--- week-7/day-48/main.py
+++ week-7/day-48/main.py
@@ -4,5 +4,7 @@ chrome_driver_path = "/Users/mjrod/Documents/coding/selenium-chrome-driver/chrom
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://amazon.ca")
+driver.get("https://python.org")
+search = driver.find_element_by_name("q")
+print(search.get_attribute("placeholder"))
 driver.quit()
