2c47b06ecd07ef0e1a74eac1fd62ddb7e0ec51c6
mjrod <michael@1mrkt.to>
2022-03-08 18:00:05 -0500

    day-55 - python-decorators arg/kwarg decorator made function to print args using decorator

--- week-8/day-55/main.py
+++ week-8/day-55/main.py
@@ -1,5 +1,16 @@
 # Create the logging_decorator() function 👇
-
+def logging_decorator(function):
+    def wrap(*args, **kwargs):
+        print(f"function executed {function.__name__}{args}")
+        result = function(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrap
 
 
 # Use the decorator 👇
+@logging_decorator
+def my_function(a, b, c):
+    return a+b+c
+
+
+my_function(1, 2, 3)
