ef85b43fc70aa7ec56f4d5bab91f78c0e6382b3b
mjrod <michael@1mrkt.to>
2022-05-03 16:37:10 -0400

    day-57 - blog_project built blog api request

--- week-8/day-57/project_blog/main.py
+++ week-8/day-57/project_blog/main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/bf0deee3eaa99ae6599c"
+    blog_response = requests.get(blog_url)
+    blog_posts = blog_response.json()
+    return render_template("index.html", posts=blog_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
