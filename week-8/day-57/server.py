b92afa33fec13e429da56ca3c915e083302db4d6
mjrod <michael@1mrkt.to>
2022-05-03 11:35:04 -0400

    day-57 - used templating to add variable into html

--- week-8/day-57/server.py
+++ week-8/day-57/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_number = random.randint(0, 10)
+    return render_template("index.html", rand_num= random_number)
 
 if __name__ == "__main__":
     app.run(debug=True)
