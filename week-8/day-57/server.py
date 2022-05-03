b87dc5b527e8eaef103e111f3aed5e0ceddb746a
mjrod <michael@1mrkt.to>
2022-05-03 11:52:38 -0400

    day-57 - built templating for new page

--- week-8/day-57/server.py
+++ week-8/day-57/server.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from flask import Flask, render_template
 import random
 import datetime
@@ -11,6 +12,10 @@ def home():
     date = datetime.datetime.now()
     current_year = date.strftime("%Y")
     return render_template("index.html", rand_num= random_number, year=current_year)
+
+@app.route("/guess/<name_input>")
+def guess(name_input):
+    return render_template("name.html", name=name_input)
 
 if __name__ == "__main__":
     app.run(debug=True)
