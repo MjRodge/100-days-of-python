2ed370c0799e407cba9fc0c5071334ea99335936
mjrod <michael@1mrkt.to>
2022-05-03 13:39:01 -0400

    day-57 - made api call for gender

--- week-8/day-57/server.py
+++ week-8/day-57/server.py
@@ -2,6 +2,7 @@ from unicodedata import name
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -11,11 +12,15 @@ def home():
     random_number = random.randint(0, 10)
     date = datetime.datetime.now()
     current_year = date.strftime("%Y")
-    return render_template("index.html", rand_num= random_number, year=current_year)
+    return render_template("index.html", rand_num=random_number, year=current_year)
 
 @app.route("/guess/<name_input>")
 def guess(name_input):
-    return render_template("name.html", name=name_input)
+    gender_url = f"https://api.genderize.io?name={name_input}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    api_gender = gender_data["gender"]
+    return render_template("name.html", name=name_input, gender=api_gender)
 
 if __name__ == "__main__":
     app.run(debug=True)
