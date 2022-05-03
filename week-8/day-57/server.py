4184d19e042a54047136b0178a9f9d42525f4c67
mjrod <michael@1mrkt.to>
2022-05-03 11:41:34 -0400

    day-57 - used programatic templating for footer

--- week-8/day-57/server.py
+++ week-8/day-57/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     random_number = random.randint(0, 10)
-    return render_template("index.html", rand_num= random_number)
+    date = datetime.datetime.now()
+    current_year = date.strftime("%Y")
+    return render_template("index.html", rand_num= random_number, year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
