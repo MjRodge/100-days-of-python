79d327e725110ae34497fb9e938861054a44c72e
mjrod <michael@1mrkt.to>
2022-05-04 09:46:31 -0400

    day-57 - blog_project added post class and links

--- week-8/day-57/project_blog/main.py
+++ week-8/day-57/project_blog/main.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
+from post import Post
 import requests
+
+posts = requests.get("https://api.npoint.io/bf0deee3eaa99ae6599c").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/bf0deee3eaa99ae6599c"
-    blog_response = requests.get(blog_url)
-    blog_posts = blog_response.json()
-    return render_template("index.html", posts=blog_posts)
+    return render_template("index.html", posts=post_objects)
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == post_id:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
