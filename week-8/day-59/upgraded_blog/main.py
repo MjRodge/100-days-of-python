9284ddba4182d1605bd845e1615f0c5f91b5584e
mjrod <michael@1mrkt.to>
2022-05-05 10:54:26 -0400

    day-59 - upgraded_blog add individual post links

--- week-8/day-59/upgraded_blog/main.py
+++ week-8/day-59/upgraded_blog/main.py
@@ -7,7 +7,6 @@ blog_posts = requests.get("https://api.npoint.io/bf0deee3eaa99ae6599c").json()
 
 @app.route("/")
 def home():
-    print(blog_posts)
     return render_template("index.html", posts=blog_posts)
 
 @app.route("/contact")
@@ -17,6 +16,14 @@ def contact():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    requested_post = None
+    for blog_post in blog_posts:
+        if blog_post["id"] == post_id:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
