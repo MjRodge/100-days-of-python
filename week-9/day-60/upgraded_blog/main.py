f12d55f610780b10d529d52530712ca6186ad724
mjrod <michael@1mrkt.to>
2022-05-05 17:01:51 -0400

    day-60 - upgraded_blog added smtplib email

--- week-9/day-60/upgraded_blog/main.py
+++ week-9/day-60/upgraded_blog/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+from secrets import my_email, my_password
 
 app = Flask("__main__")
 
@@ -18,6 +20,11 @@ def contact():
         email = request.form["email"]
         phone = request.form["phone"]
         message = request.form["message"]
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            # my_email my_password configured in secrets.py
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=my_email, to_addrs="michael@1mrkt.to", msg=f"Subject:blog contact\n\nmsg_from: {name}, email: {email}, phone: {phone}, message: {message}")
         return render_template("contact.html", msg_sent=True)
 
 @app.route("/about")
