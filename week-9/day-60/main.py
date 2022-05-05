2ed14e6d9baaa0b2010846be889023c92b54f233
mjrod <michael@1mrkt.to>
2022-05-05 15:46:04 -0400

    day-60 - added in form submit functionality

--- week-9/day-60/main.py
+++ week-9/day-60/main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    firstname = request.form["firstname"]
+    surname = request.form["surname"]
+    return f"<h1>firstname: {firstname}, surname: {surname}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
