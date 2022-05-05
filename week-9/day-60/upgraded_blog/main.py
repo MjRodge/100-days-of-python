34a6b551cd5633d2b5e6fd1b3c0d6f4e519dca5f
mjrod <michael@1mrkt.to>
2022-05-05 16:09:37 -0400

    day-60 - upgraded_blog contact entry route added

--- week-9/day-60/upgraded_blog/main.py
+++ week-9/day-60/upgraded_blog/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask("__main__")
@@ -24,6 +24,14 @@ def get_post(post_id):
         if blog_post["id"] == post_id:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    name = request.form["name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    message = request.form["message"]
+    return f"thank you {name}, we will email you here: {email}"
 
 if __name__ == "__main__":
     app.run(debug=True)
