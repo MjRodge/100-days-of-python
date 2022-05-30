a887fe252902bc91b0ef1b6bb9a6467451721d39
mjrod <michael@1mrkt.to>
2022-05-30 15:01:19 -0400

    day-64 - 10-movies added db schema

--- week-9/day-64/main.py
+++ week-9/day-64/main.py
@@ -1,3 +1,4 @@
+from pydoc import describe
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -8,7 +9,24 @@ import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movie-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 Bootstrap(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+db.create_all()
 
 
 @app.route("/")
